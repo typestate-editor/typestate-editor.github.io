1c48008cfca1551f3a4d9809d52238153a70d559
jdmota <jdmota@users.noreply.github.com>
2023-05-25 16:18:55 +0000

    Deploy website 2cd9a8ca8d40f4aa3590d4690f4db3aee950f60d

--- _next/static/zEprncl1FXCJndf76A9rb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cc9391a0-b1ccffea88d90ef4.js","static/chunks/5-03e96c7887b19810.js","static/chunks/pages/index-6c0c53151ef13665.js"],"/_error":["static/chunks/pages/_error-616b405a0790e84f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
